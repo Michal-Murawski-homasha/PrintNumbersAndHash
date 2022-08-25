d3d46a348ef12d415f152c0dfd17e7a0c5883fdf
Michał Murawski <homasha@gmail.com>
2022-08-25 16:37:54 +0200

    Initial commit

---
+++ src/PrintNumbersAndHash.java
@@ -0,0 +1,10 @@
+public class PrintNumbersAndHash {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(i + "" + i + "" + i);
+            for (int j = 1; j <= 4; j++) {
+                System.out.println("####");
+            }
+        }
+    }
+}
